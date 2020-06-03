6b6b982d5b1883ed127c64ccd051bdcbaadb80a0
ragarsa <rarosagar.igf@gmail.com>
2020-06-02 20:43:43 -0500

    Código mejorado

--- age_and_promedio.py
+++ age_and_promedio.py
@@ -1,44 +1,47 @@
 name = input("Introduce el nombre de la persona: ")
 
 age = int(input("Introduce la edad de la persona: "))
-promedio = float(input("Introduce tu promedio: "))
 
 
-if age < 6: 
-    print(name, "Está en maternal")
-    print(name, "No calificas para esta área")
+if age > 0:
+
+    if age < 6: 
+        print(name, "Está en maternal")
+        print(name, ",no calificas para esta área")
+
        
-elif age >= 6 and age < 12:
-    print(name, "Está en primaria")
-        
-    if promedio >= 9.5:
-        print("Tu promedio es sobresaliente")
-    else: 
-         print("Debes mejorar tu rendimiento")
-
-elif age >= 12 and age < 15:
-    print(name, "Está en secundaria")
-       
-    if promedio >= 9:
-        print("Tu promedio es sobresaliente")
-    else: 
-        print("Debes mejorar tu rendimiento")
-
-elif age >= 15 and age < 18:
-    print(name, "Está en preparatoria")
-         
-    if promedio >= 8.5:
-        print("Tu promedio es sobresaliente")
-    else: 
-        print("Debes mejorar tu rendimiento")
-elif age >= 18 and age < 23:
-    print(name, "Está en universidad")
-         
-    if promedio >= 8:
+    elif age >= 6 and age < 12:
+        print(name, "Está en primaria")
+        promedio = float(input("Introduce tu último promedio: "))
+        if promedio >= 9.5:
             print("Tu promedio es sobresaliente")
-    else: 
+        else: 
             print("Debes mejorar tu rendimiento")
-else: 
-    print(name, "Es profesional")
-    print("Tú no estudias")
 
+    elif age >= 12 and age < 15:
+        print(name, "Está en secundaria")
+        promedio = float(input("Introduce tu último promedio: "))
+        if promedio >= 9:
+            print("Tu promedio es sobresaliente")
+        else: 
+            print("Debes mejorar tu rendimiento")
+
+    elif age >= 15 and age < 18:
+        print(name, "Está en preparatoria")
+        promedio = float(input("Introduce tu último promedio: "))
+        if promedio >= 8.5:
+            print("Tu promedio es sobresaliente")
+        else: 
+            print("Debes mejorar tu rendimiento")
+    elif age >= 18 and age < 23:
+        print(name, "Está en universidad")
+        promedio = float(input("Introduce tu último promedio: "))
+        if promedio >= 8:
+            print("Tu promedio es sobresaliente")
+        else: 
+            print("Debes mejorar tu rendimiento")
+    else: 
+        print(name, "Es profesional")
+        print("Tú no estudias")
+else: 
+    print("Tú no has nacido o eres un bebé")
