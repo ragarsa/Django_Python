f4edc2f96d84edbaaf705de36db2c9a9dce7e605
ragarsa <rarosagar.igf@gmail.com>
2020-06-05 19:27:18 -0500

    Mejorado

--- for_examples.py
+++ for_examples.py
@@ -4,17 +4,17 @@ for i in range(0,301):
 
 for i in range(0,21):
     print("La tabla del multiplicación {}*15 es: {}".format(i,i*15) )
-par = 0
+
+impar = 0
 for i in range(50,101):
     if i % 2 != 0:
-        par += i
-print("La súma de números impares de 50 a 100 es: ",par)
+        impar += i
+print("La súma de números impares de 50 a 100 es: ", impar)
     
-impar=0
+par = 0
 for i in range(10,101):
     if i % 2 == 0:
-        impar += i
-        
-print("La suma de pares de 10 a 100 es: ",impar)
+        par += i
+print("La suma de pares de 10 a 100 es: ", par)
 
     
